6369a65e19769e30853536fd70bb44982c86740a
JustNawaf <n-f555@hotmail.com>
2020-09-25 19:09:37 +0300

    Update

--- database/migrations/2020_09_25_140331_create_items_table.php
+++ database/migrations/2020_09_25_140331_create_items_table.php
@@ -15,9 +15,12 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->boolean('selected');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
